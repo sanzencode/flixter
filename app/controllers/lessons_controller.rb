9b106f5738c6365ef88b5842ebcdc20f6c13ac3b
Sandra Harrasser <design@sandraharrasser.com>
2015-12-22 00:27:03 +0000

    added req enrollment with allert. DONE

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,15 +1,17 @@
 class LessonsController < ApplicationController
-	#before_action :require_enrollment_for_current_lesson
+	before_action :require_enrollment_for_current_lesson, :only => [:show]
 
 	def show
 	end
 
 	private
 
-	#def require_enrollment_for_current_lesson
-		#if current_lesson.section.course.user != current_user
-		#	return render :text => 'Unauthorized', :status => :Unauthorized
-		#end
+	def require_enrollment_for_current_lesson
+		this_course = current_lesson.section.course
+		if !current_lesson.section.course.user.enrolled_in?(this_course)
+			redirect_to courses_path(this_course), :alert => 'Please Enroll For Access to Lessons'
+		end
+	end
 
 	helper_method :current_lesson
 	def current_lesson
